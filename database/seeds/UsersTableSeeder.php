8f92ebf40b733b12f702664fd0da41e6ef42f358
John <juaorok@hotmail.com>
2019-08-29 08:04:41 -0300

    Update UsersTableSeeder.php

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,7 +15,7 @@ class UsersTableSeeder extends Seeder
             [
                 'name' => 'Eduardo Henrique Silva',
                 'email' => 'eduardo.silva@imob.com.br',
-                'password' => bcrypt('q1q2q3'),
+                'password' => bcrypt('e1e2e3'),
                 'avatar' => '364a440226e1b575411a0e324e712d17.jpg',
                 'job' => 'Corretor',
                 'about' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec maximus nisi a nisi venenatis, sed condimentum lectus luctus. Integer felis augue, dignissim eget rhoncus nec, elementum et lorem.'
@@ -23,7 +23,7 @@ class UsersTableSeeder extends Seeder
             [
                 'name' => 'Guilherme de Paula',
                 'email' => 'guilherme.paula@imob.com.br',
-                'password' => bcrypt('u1u2u3'),
+                'password' => bcrypt('g1g2g3'),
                 'avatar' => 'd6ee4e61af68a10f9bb9ee130313881f.jpg',
                 'job' => 'Corretor',
                 'about' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec maximus nisi a nisi venenatis, sed condimentum lectus luctus. Integer felis augue, dignissim eget rhoncus nec, elementum et lorem.'
@@ -31,7 +31,7 @@ class UsersTableSeeder extends Seeder
             [
                 'name' => 'Pedro Rodrigues Almeida',
                 'email' => 'pedro.almeida@imob.com.br',
-                'password' => bcrypt('g1g2g3'),
+                'password' => bcrypt('p1p2p3'),
                 'avatar' => '38e2b2e31c0fce9537f735dda9fdf10a.jpg',
                 'job' => 'Corretor',
                 'about' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec maximus nisi a nisi venenatis, sed condimentum lectus luctus. Integer felis augue, dignissim eget rhoncus nec, elementum et lorem.'
@@ -39,7 +39,7 @@ class UsersTableSeeder extends Seeder
             [
                 'name' => 'Roberto de Souza Santos',
                 'email' => 'roberto.santos@imob.com.br',
-                'password' => bcrypt('m1m2m3'),
+                'password' => bcrypt('r1r2r3'),
                 'avatar' => '7aa99682f9d3a129f54e0eae9ccd3628.jpg',
                 'job' => 'Corretor',
                 'about' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec maximus nisi a nisi venenatis, sed condimentum lectus luctus. Integer felis augue, dignissim eget rhoncus nec, elementum et lorem.'
